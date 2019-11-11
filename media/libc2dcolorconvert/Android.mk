450d3a1a97ff0d2fabc43d8d0636c0681567073c
EVNIT <evnithn@gmail.com>
2019-11-11 08:42:23 +0700

    Driver for Q

---
+++ media/libc2dcolorconvert/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        C2DColorConverter.cpp
+
+LOCAL_CFLAGS += -Wno-error
+
+LOCAL_C_INCLUDES := \
+    $(TARGET_OUT_HEADERS)/qcom/display
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+LOCAL_CFLAGS += -Wno-error
+
+LOCAL_SHARED_LIBRARIES := libutils liblog libdl
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := libc2dcolorconvert
+
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+include $(BUILD_SHARED_LIBRARY)
